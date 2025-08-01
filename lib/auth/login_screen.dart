05db5fe2cbb4c830a260064de6fa748766c92bae
HassanNasser <hassanmsr03@gmail.com>
2025-08-01 23:40:05 +0300

    Add validation to Auth forms & visibility password

--- lib/auth/login_screen.dart
+++ lib/auth/login_screen.dart
@@ -16,64 +16,82 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/Logo.png',
-              height: MediaQuery.sizeOf(context).height * 0.2,
-              fit: BoxFit.fill,
-            ),
-            SizedBox(height: 24),
-            CustomTextFormField(
-              hintText: 'Email',
-              prefixIconImageName: 'email',
-              controller: emailController,
-            ),
-            SizedBox(height: 16),
-            CustomTextFormField(
-              hintText: 'Password',
-              prefixIconImageName: 'password',
-              controller: passwordController,
-            ),
-            SizedBox(height: 24),
-            DefaultElevatedButton(onpressed: login, lable: 'Login'),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Don\'t have an account?',
-                  style: Theme.of(
-                    context,
-                  ).textTheme.titleMedium!.copyWith(color: AppTheme.black),
-                ),
-                TextButton(
-                  onPressed: () => Navigator.of(
-                    context,
-                  ).pushReplacementNamed(RegesterScreen.routeName),
-                  child: Text('Create Account'),
-                ),
-              ],
-            ),
-          ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/Logo.png',
+                height: MediaQuery.sizeOf(context).height * 0.2,
+                fit: BoxFit.fill,
+              ),
+              SizedBox(height: 24),
+              CustomTextFormField(
+                hintText: 'Email',
+                prefixIconImageName: 'email',
+                controller: emailController,
+                validator: (value) {
+                  if (value == null || value.length < 5) {
+                    return 'Enter valid email';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              CustomTextFormField(
+                hintText: 'Password',
+                prefixIconImageName: 'password',
+                ispassword: true,
+                controller: passwordController,
+                validator: (value) {
+                  if (value == null || value.length < 8) {
+                    return 'password must be at least 8 characters';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 24),
+              DefaultElevatedButton(onpressed: login, lable: 'Login'),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Don\'t have an account?',
+                    style: Theme.of(
+                      context,
+                    ).textTheme.titleMedium!.copyWith(color: AppTheme.black),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.of(
+                      context,
+                    ).pushReplacementNamed(RegesterScreen.routeName),
+                    child: Text('Create Account'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
   void login() {
-    FirebaseServices.login(
-      email: emailController.text,
-      password: passwordController.text,
-    ).then((user){
-      Navigator.of(context).pushReplacementNamed(HomeScreen.routename);
-    });
+    if (formKey.currentState!.validate()) {
+      FirebaseServices.login(
+        email: emailController.text,
+        password: passwordController.text,
+      ).then((user) {
+        Navigator.of(context).pushReplacementNamed(HomeScreen.routename);
+      });
+    }
   }
 }
--- lib/auth/regester_screen.dart
+++ lib/auth/regester_screen.dart
@@ -18,72 +18,100 @@ class _RegesterScreenState extends State<RegesterScreen> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController nameController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/Logo.png',
-              height: MediaQuery.sizeOf(context).height * 0.2,
-              fit: BoxFit.fill,
-            ),
-            SizedBox(height: 24),
-            CustomTextFormField(
-              hintText: 'Name',
-              prefixIconImageName: 'person',
-              controller: nameController,
-            ),
-            SizedBox(height: 16),
-            CustomTextFormField(
-              hintText: 'Email',
-              prefixIconImageName: 'email',
-              controller: emailController,
-            ),
-            SizedBox(height: 16),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/Logo.png',
+                height: MediaQuery.sizeOf(context).height * 0.2,
+                fit: BoxFit.fill,
+              ),
+              SizedBox(height: 24),
+              CustomTextFormField(
+                hintText: 'Name',
+                prefixIconImageName: 'person',
+                controller: nameController,
+                validator: (value) {
+                  if (value == null || value.length < 3) {
+                    return 'Enter valid name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              CustomTextFormField(
+                hintText: 'Email',
+                prefixIconImageName: 'email',
+                controller: emailController,
+                validator: (value) {
+                  if (value == null || value.length < 5) {
+                    return 'Enter valid email';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
 
-            CustomTextFormField(
-              hintText: 'Password',
-              prefixIconImageName: 'password',
-              controller: passwordController,
-            ),
-            SizedBox(height: 24),
-            DefaultElevatedButton(onpressed: register, lable: 'Create Account'),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Already Have Account ?',
-                  style: Theme.of(
-                    context,
-                  ).textTheme.titleMedium!.copyWith(color: AppTheme.black),
-                ),
-                TextButton(
-                  onPressed: () => Navigator.of(
-                    context,
-                  ).pushReplacementNamed(LoginScreen.routeName),
-                  child: Text('Login'),
-                ),
-              ],
-            ),
-          ],
+              CustomTextFormField(
+                hintText: 'Password',
+                prefixIconImageName: 'password',
+                controller: passwordController,
+                ispassword: true,
+                validator: (value) {
+                  if (value == null || value.length < 8) {
+                    return 'password must be at least 8 characters';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 24),
+              DefaultElevatedButton(
+                onpressed: register,
+                lable: 'Create Account',
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already Have Account ?',
+                    style: Theme.of(
+                      context,
+                    ).textTheme.titleMedium!.copyWith(color: AppTheme.black),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.of(
+                      context,
+                    ).pushReplacementNamed(LoginScreen.routeName),
+                    child: Text('Login'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
   void register() {
-    FirebaseServices.registerUser(
-      name: nameController.text,
-      email: emailController.text,
-      password: passwordController.text,
-    ).then((user){
-      Navigator.of(context).pushReplacementNamed(HomeScreen.routename);
-    });
+    if (formKey.currentState!.validate()) {
+      FirebaseServices.registerUser(
+        name: nameController.text,
+        email: emailController.text,
+        password: passwordController.text,
+      ).then((user) {
+        Navigator.of(context).pushReplacementNamed(HomeScreen.routename);
+      });
+    }
   }
 }
